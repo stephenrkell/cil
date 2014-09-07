74277db94f6584b37b9c949d799c061152020f74
Stephen Kell <stephen.kell@cl.cam.ac.uk>
2014-09-07 14:16:53 +0100

    Allow preemption of built-in functions
    Previously, if the user defined a function, say strstr(), which also has
    a builtin version, __builtin_strstr(), but the file contained calls only
    to the builtin version, the user's version would be ignored. This is a
    divergence from gcc's behaviour, where the user's function would be
    called instead of the builtin (even for calls directly to the builtin).
    The divergence is caused by CIL deleting the apparently unused user
    function. Fix it by marking the user-defined function as referenced
    whenever the builtin is marked.

--- test/testcil.pl
+++ test/testcil.pl
@@ -567,6 +567,7 @@ addTest("test/sync-3 _GNUCC=1");
 addTest("testrun/comparisons");
 addTest("testrun/assign");
 addTest("test/vector_compound_init");
+addTest("testrun/preempt-builtin");    
 
 
 
