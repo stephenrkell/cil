295b0532da4c38d4c2c3375f9f7307d8685c4bb9
Stephen Kell <stephen.kell@kcl.ac.uk>
2024-04-24 17:18:44 +0100

    cabs2cil: try to get the right semantics for 'const'-qualified parameters of type typedef-of-array

--- src/frontc/cabs2cil.ml
+++ src/frontc/cabs2cil.ml
@@ -3207,13 +3207,18 @@ and doType (nameortype: attributeClass) (* This is AttrName if we are doing
           match args with
             [] -> ()
           | a :: args' -> 
-              (match unrollType a.vtype with
-                TArray(bt,lo,attr) -> 
+              (* C is weird: if our argument is CV-qualified "const", but is
+               * of array type, the "const" goes on the pointer *target*. *)
+              (match (unrollType a.vtype, filterAttributes "const" a.vattr) with
+                (TArray(bt,lo,attr), []) -> 
                   (* Note that for multi-dimensional arrays we strip off only
                      the first TArray and leave bt alone. *)
                   a.vtype <- turnArrayIntoPointer bt lo attr
-              | TFun _ -> a.vtype <- TPtr(a.vtype, [])
-              | TComp (comp, _) -> begin
+              | (TArray(bt,lo,attr), _) -> (* same again but we move the 'const *) 
+                  a.vtype <- turnArrayIntoPointer bt lo ((Attr ("const", [])) :: attr);
+                  a.vattr <- dropAttribute "const" a.vattr
+              | (TFun _, _) -> a.vtype <- TPtr(a.vtype, [])
+              | (TComp (comp, _), _) -> begin
                   match isTransparentUnion a.vtype with
                     None ->  ()
                   | Some fstfield -> 
@@ -3221,7 +3226,7 @@ and doType (nameortype: attributeClass) (* This is AttrName if we are doing
                          (argidx, a.vtype) :: !transparentUnionArgs;
                       a.vtype <- fstfield.ftype;
               end
-              | _ -> ());
+              | (_, _) -> ());
               fixupArgumentTypes (argidx + 1) args'
         in
         let args = 
