54f841a65cee8a99992560588180846bed632eb5
Simmo Saan <simmo.saan@gmail.com>
2026-01-15 10:16:01 +0200

    Add tests for _Static_assert without message

--- test/small1/c11-static-assert.c
+++ test/small1/c11-static-assert.c
@@ -1,10 +1,11 @@
 #include "testharness.h"
 #include <stdnoreturn.h>
 
-_Static_assert (2  <= 18, "blubb");
-
+_Static_assert (2 <= 18);
+_Static_assert (2 <= 18, "blubb");
 
 int main() {
-    _Static_assert (2  <= 18, "blubb");
+    _Static_assert (2 <= 18);
+    _Static_assert (2 <= 18, "blubb");
     SUCCESS;
 }
